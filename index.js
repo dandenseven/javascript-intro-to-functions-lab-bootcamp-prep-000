e21cc39f24cc2060aa7a448715af31c9b6877ee6
Dane Knight <knight1seven@gmail.com>
2017-08-30 17:53:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,5 +21,6 @@ function  sayHiToGrandma(string) {
   if (uppercase.toUpperCase() === uppercase){
     return ("YES INDEED!")
 function sayHiToGrandma(string) {
-  if (string === "I love you, too")
-        return ("I love you, Grandma.")
+  if (string === "I love you, Grandma")
+        return ("I love you, too.")
+}
