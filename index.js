382b0b11093b756c94baaf8ba6bbdb76e0702a13
Dane Knight <knight1seven@gmail.com>
2017-08-30 18:40:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,21 +6,21 @@ return string.toLowerCase()
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase())
+  console.log(shout(string))
 }
 logShout("string")
 function logWhisper(string) {
-  console.log(string.toLowerCase())
+  console.log(whisper(string))
 }
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string){
-    return ("I can't hear you!")
+    return "I can't hear you!"
   }
   else if (string.toUpperCase() === string){
-    return ("YES INDEED!")
+    return "YES INDEED!"
   }
   else if (string === "I love you, Grandma."){
-        return ("I love you, too.")
+    return "I love you, too."
   }
 }
